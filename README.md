b870ed6b7cf2bbe9c065ba8505f141b20cb0748f
Kazuyuki Kohno <kkohno@twitter.com>
2013-09-26 18:16:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+SuperNavigator
+========
+
+Sublime Text plugin to navigate you in current file.
+
+## Usage
+
+The `super_navigate` command is binded to `super+k + super+a`.
---
+++ SuperNavigator.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin
+import re
+
+class SuperNavigateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        regions = self.view.find_all('.+')
+        items = list(map(lambda _: self.view.substr(self.view.line(_)), regions))
+
+        def on_done(index):
+            if index >= 0:
+                region = regions[index]
+                self.view.sel().clear()
+                self.view.sel().add(region)
+                self.view.show(region)
+
+        self.view.window().show_quick_panel(items, on_done, sublime.MONOSPACE_FONT, -1, on_done)
