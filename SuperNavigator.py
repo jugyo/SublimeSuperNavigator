b9351089b1f4f6d8abc5d86c5d2807cfd477a867
Kazuyuki Kohno <kkohno@twitter.com>
2013-09-27 16:19:59 +0900

    use selected text to filter the document

--- SuperNavigator.py
+++ SuperNavigator.py
@@ -2,7 +2,12 @@ import sublime, sublime_plugin
 
 class SuperNavigateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        regions = self.view.find_all('.+')
+        if len(self.view.sel()) == 1 and self.view.sel()[0].size() > 0:
+            pattern = self.view.substr(self.view.sel()[0])
+            regions = self.view.find_all(".*%s.*" % pattern)
+        else:
+            regions = self.view.find_all('.+')
+
         items = [self.view.substr(self.view.line(_)) for _ in regions]
 
         def on_done(index):
